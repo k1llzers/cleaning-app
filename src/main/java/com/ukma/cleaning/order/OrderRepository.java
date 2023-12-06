0de97c6934ccca1bce2ab7be4c69aa4dc82a1b90
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-12-06 23:17:51 +0200

    add new methods to OrderRepository and UserRepository

--- src/main/java/com/ukma/cleaning/order/OrderRepository.java
+++ src/main/java/com/ukma/cleaning/order/OrderRepository.java
@@ -2,6 +2,7 @@ package com.ukma.cleaning.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
@@ -9,4 +10,6 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     List<OrderEntity> findAllByStatusNotAndClientIdIs(Status status, Long clientId);
     // for admin to get all needed orders by status
     List<OrderEntity> findAllByStatus(Status status);
+    // for available booking time
+    List<OrderEntity> findAllByOrderTimeBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/ukma/cleaning/user/UserRepository.java
+++ src/main/java/com/ukma/cleaning/user/UserRepository.java
@@ -2,8 +2,11 @@ package com.ukma.cleaning.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findUserEntityByEmail(String email);
+
+    List<UserEntity> findAllByRole(Role role);
 }
