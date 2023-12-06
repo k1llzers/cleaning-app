4d808bf06087b2f37bb3634fcd1818b9e06683de
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-12-06 13:26:50 +0200

    change pom

--- src/test/java/com/ukma/cleaning/CleaningApplicationTests.java
+++ src/test/java/com/ukma/cleaning/CleaningApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CleaningV2ApplicationTests {
+class CleaningApplicationTests {
 
 	@Test
 	void contextLoads() {
