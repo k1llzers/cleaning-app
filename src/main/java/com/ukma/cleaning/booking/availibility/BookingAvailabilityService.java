5ce2c2afdc20a25daca5e661c38f9f690fd9a9ea
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-12-06 22:53:53 +0200

    fix ReviewMapper

---
+++ src/main/java/com/ukma/cleaning/booking/availibility/BookingAvailabilityService.java
@@ -0,0 +1,9 @@
+package com.ukma.cleaning.booking.availibility;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Map;
+
+public interface BookingAvailabilityService {
+//    Map<LocalDate, Time>
+}
--- src/main/java/com/ukma/cleaning/utils/mappers/ReviewMapper.java
+++ src/main/java/com/ukma/cleaning/utils/mappers/ReviewMapper.java
@@ -4,9 +4,11 @@ import com.ukma.cleaning.review.ReviewDto;
 import com.ukma.cleaning.review.ReviewEntity;
 import com.ukma.cleaning.utils.configuration.MapperConfig;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface ReviewMapper {
+    @Mapping(target = "orderId", source = "id")
     ReviewDto toDto(ReviewEntity review);
 
     ReviewEntity toEntity(ReviewDto reviewDto);
