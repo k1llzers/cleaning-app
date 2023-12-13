c6ae6d866741d9219bed0675b78a1f8a68b35dc9
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-12-13 17:16:39 +0200

    refactor timezone

--- src/main/java/com/ukma/cleaning/order/OrderServiceImpl.java
+++ src/main/java/com/ukma/cleaning/order/OrderServiceImpl.java
@@ -16,9 +16,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Slf4j
